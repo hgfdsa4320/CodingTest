4621073a8ebcee90f45e7092de7e973551b7fa87
hgfdsa4320 <80511046+hgfdsa4320@users.noreply.github.com>
2023-05-01 16:21:02 +0900

    [level 2] Title: 땅따먹기, Time: 10.65 ms, Memory: 86.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,22 @@
+class Solution {
+    int solution(int[][] land) {
+        int answer = 0;
+
+        for(int i=1;i<land.length;i++){
+            for(int j=0;j<4;j++){
+                int max = 0;
+                for(int k=0;k<4;k++){
+                    if(k!=j && land[i-1][k]>max){
+                        max = land[i-1][k];
+                    }
+                }
+                land[i][j]+=max;
+            }
+        }
+        for(int i=0;i<4;i++){
+            answer = Math.max(answer,land[land.length-1][i]);
+        }
+
+        return answer;
+    }
+}
