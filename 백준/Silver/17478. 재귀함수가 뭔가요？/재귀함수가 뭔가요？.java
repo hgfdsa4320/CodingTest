42bfe7c5aada4a582077aa4e8c4478313500005d
hgfdsa4320 <80511046+hgfdsa4320@users.noreply.github.com>
2023-05-03 21:37:13 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 152 ms, Memory: 14884 KB -BaekjoonHub

---
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+public class Main{
+    static int n;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
+
+        String s1 = "\"재귀함수가 뭔가요?\"";
+        String s2 = "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.";
+        String s3 = "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.";
+        String s4 = "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"";
+        String s5 = "라고 답변하였지.";
+        dfs(0,s1,s2,s3,s4,s5);
+        System.out.println(s5);
+        
+    }
+    static void dfs(int cnt,String s1,String s2,String s3,String s4,String s5){
+        if(cnt==n){
+            String tmp = "____";
+            String res="";
+            for(int i=0;i<n;i++){
+                res+=tmp;
+            }
+            System.out.println(s1);
+            System.out.println(res+"\"재귀함수는 자기 자신을 호출하는 함수라네\"");
+
+        }else{
+            System.out.println(s1);
+            System.out.println(s2);
+            System.out.println(s3);
+            System.out.println(s4);
+            dfs(cnt+1,"____"+s1,"____"+s2,"____"+s3,"____"+s4,"____"+s5);
+            System.out.println("____"+s5);
+        }
+
+    }
+}
