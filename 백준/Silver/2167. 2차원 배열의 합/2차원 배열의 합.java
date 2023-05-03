c46816e7fadc6acc2e5bb8e97d6f372961371561
hgfdsa4320 <80511046+hgfdsa4320@users.noreply.github.com>
2023-05-03 23:19:54 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 2428 ms, Memory: 47808 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -5,6 +5,7 @@ public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int[][] map = new int[n+1][m+1];
@@ -28,8 +29,8 @@ public class Main{
                     answer+=map[q][w];
                 }
             }
-            System.out.println(answer);
+            sb.append(answer).append("\n");
         }
-
+        System.out.println(sb.toString());
     }
 }
