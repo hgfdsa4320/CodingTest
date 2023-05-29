0b99f8994cac4fe845bceac391fb93b7684a0a99
hgfdsa4320 <80511046+hgfdsa4320@users.noreply.github.com>
2023-05-29 15:31:02 +0900

    [Silver I] Title: 미로 탐색, Time: 136 ms, Memory: 14896 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,49 +2,46 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
-    static int[][] graph;
-    static int[] dx;
-    static int[] dy;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {1,-1,0,0};
+    static int[] dy = {0,0,1,-1};
     static int n,m;
-    
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        graph = new int[n][m];
-        dx = new int[]{1,0,-1,0};
-        dy = new int[]{0,1,0,-1};
-        
+        map = new int[n][m];
+        visited = new boolean[n][m];
         for(int i=0;i<n;i++){
-            String tmp = br.readLine();
+            String s = br.readLine();
             for(int j=0;j<m;j++){
-                graph[i][j]= tmp.charAt(j)-'0';
+                map[i][j] = s.charAt(j)-'0';
             }
         }
-    
-        System.out.println(bfs(0,0,1));
+        System.out.println(bfs());
+
     }
-    
-    static int bfs(int x, int y, int now){
+    static int bfs(){
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{x,y,now});
-        graph[x][y]=0;
+        visited[0][0]=true;
+        q.offer(new int[]{1,0,0});
         while(!q.isEmpty()){
-            int[] arr = q.poll();
-            x = arr[0];
-            y = arr[1];
-            now = arr[2];
+            int[] tmp = q.poll();
+            int cnt = tmp[0];
+            int x = tmp[1];
+            int y = tmp[2];
             if(x==n-1 && y==m-1){
-                return now;
+                return cnt;
             }
-            
             for(int i=0;i<4;i++){
                 int nx = x+dx[i];
                 int ny = y+dy[i];
-                if(nx>=0 && nx<n && ny>=0 && ny<m && graph[nx][ny]==1){
-                    graph[nx][ny]=0;
-                    q.offer(new int[]{nx,ny,now+1});
+                if(nx>=0 && nx<n && ny>=0 && ny<m && !visited[nx][ny] && map[nx][ny]==1){
+                    visited[nx][ny]=true;
+                    q.offer(new int[]{cnt+1,nx,ny});
                 }
             }
         }
