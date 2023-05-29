454a98873400e698f0a31a3d5d79cb1bcac61ba4
hgfdsa4320 <80511046+hgfdsa4320@users.noreply.github.com>
2023-05-29 11:39:36 +0900

    [Silver II] Title: DFS와 BFS, Time: 332 ms, Memory: 20696 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -2,62 +2,59 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
-    static boolean[] checked;
-    static ArrayList<Integer>[] arr;
+    static ArrayList<Integer>[] map;
+    static boolean[] visited;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int v = Integer.parseInt(st.nextToken());
-        arr = new ArrayList[n+1];
-        checked = new boolean[n+1];
+        map = new ArrayList[n+1];
+        visited = new boolean[n+1];
         for(int i=1;i<=n;i++){
-            arr[i] = new ArrayList<>();
+            map[i] = new ArrayList<>();
         }
-
         for(int i=0;i<m;i++){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            arr[a].add(b);
-            arr[b].add(a);
+            map[a].add(b);
+            map[b].add(a);
         }
-        for (int i = 1; i < arr.length; i++) {
-            Collections.sort(arr[i]);
+        for (int i = 1; i <= n; i++) {
+            Collections.sort(map[i]);
         }
-
         dfs(v);
-        checked = new boolean[n+1];
+        visited = new boolean[n+1];
         System.out.println();
         bfs(v);
     }
     static void dfs(int v){
-        checked[v]=true;
+        visited[v] = true;
         System.out.print(v+" ");
-        for(int i=0;i<arr[v].size();i++){
-            int tmp = arr[v].get(i);
-            if(!checked[tmp]){
+        for(int i=0;i<map[v].size();i++){
+            int tmp = map[v].get(i);
+            if(!visited[tmp]){
                 dfs(tmp);
             }
         }
     }
-
     static void bfs(int v){
         Queue<Integer> q = new LinkedList<>();
-        checked[v]=true;
+        visited[v] = true;
         q.offer(v);
 
         while(!q.isEmpty()){
-            int tmp = q.poll();
-            System.out.print(tmp+" ");
-            for (int i = 0; i < arr[tmp].size(); i++) {
-                if(!checked[arr[tmp].get(i)]){
-                    checked[arr[tmp].get(i)]=true;
-                    q.offer(arr[tmp].get(i));
+            v = q.poll();
+            System.out.print(v+" ");
+            for(int i=0;i<map[v].size();i++){
+                int tmp = map[v].get(i);
+                if(!visited[tmp]){
+                    visited[tmp] = true;
+                    q.offer(tmp);
                 }
             }
-
         }
     }
 }
