457adebf9a4853a24a589ed96367940781398871
hgfdsa4320 <80511046+hgfdsa4320@users.noreply.github.com>
2023-06-17 16:31:27 +0900

    [level 2] Title: 후보키, Time: 0.43 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42890. 후보키/후보키.java
+++ 프로그래머스/lv2/42890. 후보키/후보키.java
@@ -1,74 +1,41 @@
 import java.util.*;
 
 class Solution {
-    static List<int[]> list; // 후보키 후보를 담을 리스트
-    static List<int[]> res; // 후보키를 담을 리스트
-    static boolean[] visited;
-    static int index; // dfs에서 중복 제외하기 위한 인덱스
-    static int n; // 칼럼 수
     public int solution(String[][] relation) {
-        res = new ArrayList<>();
-        n = relation[0].length;
-        for(int i=1;i<=n;i++){
-            list = new ArrayList<>();
-            visited = new boolean[n];
-            index = 0;
-            dfs(i,0); // 칼럼수가 i개인 후보키 후보를 리스트에 저장
-        
-            Loop1:
-            for(int[] tmp : list){
-                //유일성 검증을 위한 Set ex)이름+전공일 경우 -> ryan music으로 저장
-                Set<String> set = new HashSet<>(); 
-                for(int j=0;j<res.size();j++){
-                    if(contains(tmp,res.get(j))) //최소성이 아니면 continue
-                        continue Loop1;
+        boolean[] checked = new boolean[relation[0].length]; 
+        int answer = 0;
+         
+        for(int i=1;i<=relation[0].length;i++){ // 칼럼 수
+            List<Integer> list = new ArrayList<>(); // 현재 후보키가 될 수 있을 지 검사할 칼럼들
+            int idx=0;
+            // 후보키가 될 칼럼들을 선택 하는 반복문 ex) i가 2이면 칼럼 두개를 찾는과정->이름,전공
+            while(idx<relation[0].length && list.size() < i){ 
+                if(!checked[idx]){
+                    list.add(idx);
                 }
-                for(int j=0;j<relation.length;j++){
-                    StringBuilder sb = new StringBuilder(); 
-                    for(int k=0;k<tmp.length;k++){
-                        sb.append(relation[j][tmp[k]]).append(" ");
+                idx++;
+            }
+            if(list.size()<i) continue;
+            boolean isPossible = true;
+            Loop:
+            for(int j=0;j<relation.length;j++){
+                Set<String> set = new HashSet<>();
+                for(int k=0;k<list.size();k++){
+                    if(set.contains(relation[j][list.get(k)])){
+                        isPossible = false;
+                        break Loop;
                     }
-                    if(set.contains(sb.toString())){ //유일성이 아니면 continue
-                        continue Loop1;
-                    }
-                    set.add(sb.toString());
-                }
-                res.add(tmp);
-            }            
-        }
-        return res.size();
-    }
-    static void dfs(int v,int cnt){
-        if(cnt==v){
-            int[] tmp = new int[v];
-            int idx = 0;
-            for(int i=0;i<n;i++){
-                if(visited[i]){
-                    tmp[idx++] = i;
+                    set.add(relation[j][list.get(k)]);
                 }
             }
-            list.add(tmp);
-        }
-        for(int i=index;i<n;i++){
-            if(!visited[i]){
-                visited[i]= true;
-                dfs(v,cnt+1);
-                visited[i]=false;
-                index = i+1;
-            }
-        }
-    }
-    static boolean contains(int[] a, int[] b){ //a가 b를 포함하고 있는지 판단하는 함수
-        for(int i=0;i<b.length;i++){
-            boolean isOkay = false;
-            for(int j=0;j<a.length;j++){
-                if(a[j]==b[i]){
-                    isOkay=true;
-                    break;
+            
+            if(isPossible) {
+                answer++;
+                for(int k=0;k<list.size();k++){
+                    checked[list.get(k)]=true;
                 }
             }
-            if(!isOkay) return false;
         }
-        return true;
+        return answer;
     }
 }
