3d06fbeda63c189fd6ca335bb3ad3713f65485bd
hgfdsa4320 <80511046+hgfdsa4320@users.noreply.github.com>
2023-08-03 15:48:03 +0900

    [Silver II] Title: DNA 비밀번호, Time: 348 ms, Memory: 22344 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,52 +1,78 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        char[] arr = {'A','C','G','T'};
-        int[] cnt = new int[4]; 
-        int[] dna = new int[4];
-        int s = Integer.parseInt(st.nextToken());
-        int p = Integer.parseInt(st.nextToken());
-        
-        String str = br.readLine();
-        st = new StringTokenizer(br.readLine());
-        for(int i=0;i<4;i++){
-            dna[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        int start = 0;
-        int answer = 0;
-        Loop1:
-        for(int end=0;end<s;end++){
-            for(int i=0;i<4;i++){
-                if(str.charAt(end)==arr[i]){
-                    cnt[i]++;
-                    break;
-                }
-            }
-            if(end>=p){   
-                for(int i=0;i<4;i++){
-                    if(str.charAt(start)==arr[i]){
-                        cnt[i]--;
-                        break;
-                    } 
-                }
-                start++; 
-            }
-            if(end>=p-1){
-                for(int i=0;i<4;i++){
-                    if(cnt[i]<dna[i]){
-                        continue Loop1;
-                    }
-                }
-                answer++;
-            }
+public class Main {
+	static int s, p;
+	static int[] cnt;
+	static String dna;
 
-            
-        }
-        System.out.println(answer);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		s = Integer.parseInt(st.nextToken());
+		p = Integer.parseInt(st.nextToken());
+		int answer = 0;
+		dna = br.readLine();
+
+		Character[] c = { 'A', 'C', 'G', 'T' };
+		st = new StringTokenizer(br.readLine());
+		cnt = new int[4];
+		for (int i = 0; i < 4; i++) {
+			cnt[i] = Integer.parseInt(st.nextToken());
+		}
+	
+		for (int i = 0; i < p; i++) {
+			if (dna.charAt(i) == 'A') {
+				cnt[0]--;
+			} else if (dna.charAt(i) == 'C') {
+				cnt[1]--;
+			} else if (dna.charAt(i) == 'G') {
+				cnt[2]--;
+			} else if (dna.charAt(i) == 'T') {
+				cnt[3]--;
+			}
+		}
+		if (isPossible())
+			answer++;
+		int start = 0;
+		for (int end = p; end < s; end++,start++) {
+			if (dna.charAt(start) == 'A') {
+				cnt[0]++;
+			} else if (dna.charAt(start) == 'C') {
+				cnt[1]++;
+			} else if (dna.charAt(start) == 'G') {
+				cnt[2]++;
+			} else if (dna.charAt(start) == 'T') {
+				cnt[3]++;
+			}
+			
+			if (dna.charAt(end) == 'A') {
+				cnt[0]--;
+			} else if (dna.charAt(end) == 'C') {
+				cnt[1]--;
+			} else if (dna.charAt(end) == 'G') {
+				cnt[2]--;
+			} else if (dna.charAt(end) == 'T') {
+				cnt[3]--;
+			}
+			if (isPossible()) answer++;
+			
+		}
+
+		System.out.println(answer);
+	}
+
+	static boolean isPossible() {
+
+		boolean isOkay = true;
+		for (int i = 0; i < 4; i++) {
+			if (cnt[i] > 0) {
+				isOkay = false;
+			}
+		}
+		if (isOkay)
+			return true;
+		return false;
+	}
+
 }
