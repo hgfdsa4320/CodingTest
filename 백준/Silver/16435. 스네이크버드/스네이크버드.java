f81dc28984ee69636739cc2b534285419791ec60
hgfdsa4320 <80511046+hgfdsa4320@users.noreply.github.com>
2023-08-11 11:57:11 +0900

    [Silver V] Title: 스네이크버드, Time: 132 ms, Memory: 14428 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int l = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int[] arr =new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		for(int i=0;i<n;i++) {
+			if(l>=arr[i]) {
+				l++;
+			}else {
+				break;
+			}
+		}
+		System.out.println(l);
+	}
+}
