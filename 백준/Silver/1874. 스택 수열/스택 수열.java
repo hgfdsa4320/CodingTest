8522a5a84987f44c5c8af670e584592554ac14a1
hgfdsa4320 <80511046+hgfdsa4320@users.noreply.github.com>
2023-08-13 13:27:48 +0900

    [Silver II] Title: 스택 수열, Time: 356 ms, Memory: 27656 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,39 +1,34 @@
 import java.io.*;
 import java.util.*;
-
-public class Main{
+public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        for(int i=0;i<n;i++){
-            arr[i] = Integer.parseInt(br.readLine());
-        }
         StringBuilder sb = new StringBuilder();
-        Stack<Integer> stack = new Stack<>();
-        stack.push(1);
-        sb.append("+").append("\n");
-        int idx = 0;
-        for(int i=2;i<=n;i++){
-            while(!stack.isEmpty() && stack.peek()==arr[idx]){
-                stack.pop();
-                sb.append("-").append("\n");
-                idx++;
+        int n = Integer.parseInt(br.readLine());
+        boolean answer = true;
+        Stack<Integer> st = new Stack<>();
+        int j = 1;
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            while (j <= num) {
+                st.push(j);
+                sb.append("+").append("\n");
+                j++;
             }
-            stack.push(i);
-            sb.append("+").append("\n");
+            if (!st.isEmpty() && st.pop() == num) {
+                sb.append("-").append("\n");
+            } else {
+                answer = false;
+                break;
+            }
         }
-        while (!stack.isEmpty() && stack.peek()==arr[idx]) {
-            stack.pop();
-            sb.append("-").append("\n");
-            idx++;
-        }
-        if (!stack.isEmpty()) {
-            System.out.println("NO");
+        if (answer) {
+            System.out.println(sb);
         } else {
-            System.out.println(sb.toString());
+            System.out.println("NO");
         }
+
 
     }
-
 }
