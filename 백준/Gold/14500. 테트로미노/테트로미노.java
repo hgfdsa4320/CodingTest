e061c9b9b59064fd97347040e8320a33f47de9ff
hgfdsa4320 <80511046+hgfdsa4320@users.noreply.github.com>
2023-09-09 15:25:48 +0900

    [Gold IV] Title: 테트로미노, Time: 664 ms, Memory: 41248 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -1,68 +1,80 @@
 import java.io.*;
 import java.util.*;
-
-public class Main{
+public class Main {
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
     static int n,m;
-    static int[] dx = {1,0,-1,0};
-    static int[] dy = {0,1,0,-1};
-    static int[][] map;
     static boolean[][] visited;
-    static int answer;
+    static int[][] arr;
+    static int answer = 0;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        map = new int[n][m];
+        arr = new int[n][m];
         visited = new boolean[n][m];
-        answer = 0;
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0;j<m;j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                visited[i][j]=true;
-                dfs(1,map[i][j],i,j);
-                visited[i][j]=false;
+
+        for (int i = 0; i < n; i++) { //1,3,4 번 해결
+            for (int j = 0; j < m; j++) {
+                visited[i][j] = true;
+                findNum(1, arr[i][j], i, j);
+                visited[i][j] = false;
             }
         }
-        for(int i=1;i<n;i++){
-            for(int j=0;j<m-2;j++){
-                answer = Math.max(answer,map[i][j]+map[i-1][j+1]+map[i][j+1]+map[i][j+2]);
+        for (int i = 0; i < n - 1; i++) { //2번 해결
+            for (int j = 0; j < m - 1; j++) {
+                int tmp = arr[i][j] + arr[i][j + 1] + arr[i + 1][j] + arr[i + 1][j + 1];
+                answer = Math.max(answer, tmp);
             }
         }
-        for(int i=0;i<n-1;i++){
-            for(int j=0;j<m-2;j++){
-                answer = Math.max(answer,map[i][j]+map[i+1][j+1]+map[i][j+1]+map[i][j+2]);
+
+        //5번 4방향
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < m-2; j++) {
+                int tmp = arr[i][j] + arr[i][j + 1] + arr[i + 1][j + 1] + arr[i][j + 2];
+                answer = Math.max(answer, tmp);
             }
         }
-        for(int i=0;i<n-2;i++){
-            for(int j=1;j<m;j++){
-                answer = Math.max(answer,map[i][j]+map[i+1][j]+map[i+1][j-1]+map[i+2][j]);
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 1; j < m-2; j++) {
+                int tmp = arr[i][j] + arr[i + 1][j + 1] + arr[i + 1][j] + arr[i+1][j - 1];
+                answer = Math.max(answer, tmp);
             }
         }
-        for(int i=0;i<n-2;i++){
-            for(int j=0;j<m-1;j++){
-                answer = Math.max(answer,map[i][j]+map[i+1][j]+map[i+1][j+1]+map[i+2][j]);
+        for (int i = 0; i < n-2; i++) {
+            for (int j = 0; j < m-1; j++) {
+                int tmp = arr[i][j] + arr[i+1][j] + arr[i + 1][j + 1] + arr[i+2][j];
+                answer = Math.max(answer, tmp);
             }
         }
+        for (int i = 1; i < n-1; i++) {
+            for (int j = 0; j < m-1; j++) {
+                int tmp = arr[i][j] + arr[i - 1][j + 1] + arr[i][j + 1] + arr[i + 1][j + 1];
+                answer = Math.max(answer, tmp);
+            }
+        }
+
         System.out.println(answer);
     }
-    static void dfs(int cnt, int sum, int x, int y){
-        if(cnt==4){
-            answer = Math.max(answer,sum);
-        }else{
-            for(int i=0;i<4;i++){
-                int nx = x+dx[i];
-                int ny = y+dy[i];
-                if(nx>=0 && nx<n && ny>=0 && ny<m && !visited[nx][ny]){
-                    visited[nx][ny] = true;
-                    dfs(cnt+1,sum+map[nx][ny],nx,ny);
-                    visited[nx][ny] = false;
-                }
+    static void findNum(int cnt, int sum, int x, int y) {
+        if (cnt == 4) {
+            answer = Math.max(answer, sum);
+            return;
+        }
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m && !visited[nx][ny]) {
+                visited[nx][ny] = true;
+                findNum(cnt + 1, sum + arr[nx][ny], nx, ny);
+                visited[nx][ny] = false;
             }
         }
 
