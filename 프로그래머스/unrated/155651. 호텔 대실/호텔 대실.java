a3a456f3820163513cfd55281e55003f6a8e7161
hgfdsa4320 <80511046+hgfdsa4320@users.noreply.github.com>
2023-09-15 10:03:03 +0900

    [unrated] Title: 호텔 대실, Time: 8.48 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.java
@@ -2,28 +2,24 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] book_time) {
-        int answer = 0;
-        int n = book_time.length;
-         int[][] times = new int[n][2];
-        for(int i=0;i<n;i++){
-            times[i][0] = changeTime(book_time[i][0]);
-            times[i][1] = changeTime(book_time[i][1]);
-        }
-        Arrays.sort(times,(a,b)->a[0]-b[0]);
-        PriorityQueue<Integer> pq = new PriorityQueue<>(); // 대실 종료 시간+10분
-        int t = 0;
-        for(int i=0;i<n;i++){
-            while(!pq.isEmpty() && pq.peek()<=times[i][0]){
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->(a-b));
+        Arrays.sort(book_time,(a,b)->changeTime(a[0])-changeTime(b[0]));
+        pq.offer(changeTime(book_time[0][1])+10);
+        int answer = 1;
+      
+        for(int i=1;i<book_time.length;i++){
+            while(!pq.isEmpty() && pq.peek()<=changeTime(book_time[i][0])){
                 pq.poll();
             }
-            pq.offer(times[i][1]+10);
-            answer = Math.max(answer,pq.size());
+            pq.offer(changeTime(book_time[i][1])+10);
+            answer = Math.max(answer,pq.size());            
         }
         return answer;
     }
     static int changeTime(String time){
-        int h = Integer.parseInt(time.split(":")[0]);
-        int m = Integer.parseInt(time.split(":")[1]);
-        return h*60+m;
+        int hour = Integer.parseInt(time.split(":")[0]);
+        int min = Integer.parseInt(time.split(":")[1]);
+        return hour*60+min;
     }
+    
 }
