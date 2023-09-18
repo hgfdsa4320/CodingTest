b38aa65d544c61df65c5e0ea1eedc14442cb534f
hgfdsa4320 <80511046+hgfdsa4320@users.noreply.github.com>
2023-09-18 23:07:22 +0900

    [Gold IV] Title: 연구소, Time: 860 ms, Memory: 298312 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,81 +1,91 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
     static int n,m;
     static int[][] map;
-    static int[] dx = {1,0,-1,0};
-    static int[] dy = {0,1,0,-1};
-    static int answer;
-    public static void main(String[] args) throws IOException{
+    static int answer = 0;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        answer = 0;
         map = new int[n][m];
-        for(int i=0;i<n;i++){
+
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0;j<m;j++){
+            for (int j = 0; j < m; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
+
             }
         }
-        dfs(0);
+        wall(0);
         System.out.println(answer);
     }
-    static void dfs(int wall){
-        if(wall==3){
-            bfs();
-        }else{
-            for(int i=0;i<n;i++){
-                for(int j=0;j<m;j++){
-                    if(map[i][j]==0){
-                        map[i][j]=1;
-                        dfs(wall+1);
-                        map[i][j]=0;
-                    }
+    static void wall(int cnt){
+        if (cnt == 3) {
+            virus();
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] == 0) {
+                    map[i][j]=1;
+                    wall(cnt + 1);
+                    map[i][j]=0;
                 }
             }
         }
     }
 
-    static void bfs(){
+    static void virus() {
+        int[][] tmp = copy(map);
         Queue<int[]> q = new LinkedList<>();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(map[i][j]==2){
-                    q.offer(new int[]{i,j});
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (tmp[i][j] == 2) {
+                    q.offer(new int[]{i, j});
                 }
             }
         }
-        int[][] arr = new int[n][m];
-        for(int i=0;i<n;i++){
-            arr[i] = map[i].clone();
+        while (true) {
+            int len = q.size();
+            int cnt = 0;
+            for (int i = 0; i < len; i++) {
+                int x = q.peek()[0];
+                int y = q.poll()[1];
+                for (int j = 0; j < 4; j++) {
+                    int nx = x + dx[j];
+                    int ny = y + dy[j];
+                    if (nx >= 0 && nx < n && ny >= 0 && ny < m && tmp[nx][ny] == 0) {
+                        tmp[nx][ny] = 2;
+                        q.offer(new int[]{nx, ny});
+                        cnt++;
+                    }
+                }
+            }
+            if(cnt==0) break;
         }
-        while(!q.isEmpty()){
-            int[] tmp = q.poll();
-            int x = tmp[0];
-            int y = tmp[1];
-            for(int i=0;i<4;i++){
-                int nx = x+dx[i];
-                int ny = y+dy[i];
-                if(nx>=0 && nx<n && ny>=0 && ny<m && arr[nx][ny]==0){
-                    arr[nx][ny]=2;
-                    q.offer(new int[]{nx, ny});
+        int safeZone = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (tmp[i][j] == 0) {
+                    safeZone++;
                 }
             }
         }
-        answer = Math.max(answer,getCount(arr));
+        answer = Math.max(answer, safeZone);
     }
-    static int getCount(int[][] arr){
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(arr[i][j]==0){
-                    cnt++;
-                }
+
+    static int[][] copy(int[][] arr){
+        int[][] tmp = new int[arr.length][arr[0].length];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                tmp[i][j] = arr[i][j];
             }
         }
-        return cnt;
+        return tmp;
     }
 }
