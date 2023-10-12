c730461b639e287dc836a851bcd442c35d148124
hgfdsa4320 <80511046+hgfdsa4320@users.noreply.github.com>
2023-10-12 11:27:21 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 120 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(fibo(n));
+    }
+    static int fibo(int n){
+        if(n==0)
+            return 0;
+        else if (n==1)
+            return 1;
+        else
+            return fibo(n-1)+fibo(n-2);
+    }
+}
