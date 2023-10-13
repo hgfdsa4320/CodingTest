41a151fbd85691ae50adf2cb7e73c02d00587875
hgfdsa4320 <80511046+hgfdsa4320@users.noreply.github.com>
2023-10-13 14:54:10 +0900

    [Gold II] Title: 스타트 택시, Time: 292 ms, Memory: 25500 KB -BaekjoonHub

--- 백준/Gold/19238. 스타트 택시/스타트 택시.java
+++ 백준/Gold/19238. 스타트 택시/스타트 택시.java
@@ -19,7 +19,7 @@ public class Main {
 		fuel = Integer.parseInt(st.nextToken());
 		map = new int[n + 1][n + 1];
 		visited = new boolean[n + 1][n + 1];
-		info = new HashMap<>();
+		info = new HashMap<>(); //  map에 {출발 위치, 목적지} 순으로 넣어 놓음 ex) {"1,2","2,3"}
 		for (int i = 1; i <= n; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 1; j <= n; j++) {
@@ -35,10 +35,7 @@ public class Main {
 			int sy = Integer.parseInt(st.nextToken());
 			int px = Integer.parseInt(st.nextToken());
 			int py = Integer.parseInt(st.nextToken());
-			map[sx][sy] = i;
-			map[px][py] = 1000 + i;
 			info.put(sx + "," + sy, px + "," + py);
-
 		}
 		if (find()) {
 			System.out.println(fuel);
@@ -49,41 +46,40 @@ public class Main {
 
 	static boolean find() {
 		boolean isChange = true;
-		Loop1: while (m > 0 && isChange) {
+		Loop1: while (m > 0 && isChange) { //태울 사람이 있고(m==태워야 되는 사람 수),변화가 있을 동안만 실행
+			//{x,y,dist}-> 좌표와 거리가 들어가서, 현재 위치에서 거리-> x좌표-> y좌표 순으로 정렬
 			PriorityQueue<int[]> q = new PriorityQueue<>(
 					(a, b) -> a[2] == b[2] ? (a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]) : a[2] - b[2]);
 			q.offer(new int[] { x, y, 0 });
 			visited[x][y] = true;
 			isChange = false;
-			int idx = 0;
 			while (!q.isEmpty()) {				
-				int tx = q.peek()[0];
-				int ty = q.peek()[1];
-				int cnt = q.poll()[2];
-				int dist = findDistance(x, y, tx, ty);
-				if (info.containsKey(tx + "," + ty)) {
-					fuel -= dist;
+				int tx = q.peek()[0]; //현재 x좌표
+				int ty = q.peek()[1]; //현재 y좌표 
+				int cnt = q.poll()[2]; // 거리
+				if (info.containsKey(tx + "," + ty)) { //tx,ty 좌표에 택시를 탈 사람이 있으면
+					int dist = findDistance(x, y, tx, ty); // 현재 위치에서 사람이 있는 위치 까지 거리 구함
+					fuel -= dist; //연료에서 거리 빼줌
 					
-					String p = info.get(tx + "," + ty);
+					String p = info.get(tx + "," + ty); // 택시탄 사람의 목적지 위치 가져옴
 					int px = Integer.parseInt(p.split(",")[0]);
 					int py = Integer.parseInt(p.split(",")[1]);
-					dist = findDistance(tx, ty, px, py);
+					dist = findDistance(tx, ty, px, py); // 현재 사람 위치에서 목적지까지 거리 구함
 					
-					if (fuel >= dist && dist != -1) {
-						fuel += dist;
-						m--;
-						x = px;
+					if (fuel >= dist && dist != -1) { // 연료가 부족하거나 가지 못하면 return false
+						fuel += dist; //갈 수 있으면 연료 채워주고 
+						m--; // 사람 한명 빼주고
+						x = px; //택시의 현재 위치를 목적지 위치로 바꿔줌
 						y = py;
-						info.remove(tx + "," + ty);
-						isChange = true;
-						visited = new boolean[n + 1][n + 1];
-						continue Loop1;
+						info.remove(tx + "," + ty); // 택시 내린 사람 info에서 지워줌
+						isChange = true; //변화가 있으면 true로 바꿔줌
+						visited = new boolean[n + 1][n + 1]; // 방문 배열 초기화
+						continue Loop1; // 위치가 변화했으므로 첨부터 다시 시작
 					} else {
-						return false;
+						return false; 
 					}
 				}
 				
-				if(idx<cnt) idx++;
 				
 				for (int i = 0; i < 4; i++) {
 					int ntx = tx + dx[i];
@@ -95,10 +91,6 @@ public class Main {
 						}
 					}
 				}
-				
-				
-	
-
 			}
 		}
 		if (m == 0)
@@ -106,7 +98,8 @@ public class Main {
 		return false;
 	}
 
-	static int findDistance(int sx, int sy, int px, int py) {
+	// 두 점 사이의 최단 거리를 찾는 함수, 이동할 수 없으면 -1 return
+	static int findDistance(int sx, int sy, int px, int py) { 
 		boolean[][] checked = new boolean[n + 1][n + 1];
 		checked[sx][sy] = true;
 		Queue<int[]> q = new LinkedList<>();
