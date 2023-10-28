e5adeb381d7845c2b4c9962d176f424aeabfe537
hgfdsa4320 <80511046+hgfdsa4320@users.noreply.github.com>
2023-10-29 00:49:49 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 632 ms, Memory: 121012 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        List<Integer> list = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        list.add(arr[0]);
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > list.get(list.size() - 1)) {
+                list.add(arr[i]);
+            } else {
+                int idx = findIdx(list, arr[i]);
+                list.set(idx, arr[i]);
+            }
+        }
+        System.out.println(list.size());
+    }
+
+    static int findIdx(List<Integer> list, int v) {
+        int left = 0;
+        int right = list.size();
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (list.get(mid) < v) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
