593ea3b49729e6ee007e6249fa70e9d62d6920c0
권순준 <80511046+hgfdsa4320@users.noreply.github.com>
2024-01-10 08:48:01 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.62 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,36 +1,54 @@
 import java.util.*;
 
 class Solution {
-    static boolean[][] visited;
-    static int[] dx = {1,0,-1,0};
-    static int[] dy = {0,1,0,-1};
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
     
-    public int solution(int[][] maps) {
+    public int bfs(int[][] maps) {
+        boolean[][] isVisited = new boolean[maps.length][maps[0].length];
+        Queue<int[]> queue = new LinkedList<>();
+        int n = maps.length - 1;
+        int m = maps[0].length - 1;
         
-        visited = new boolean[maps.length][maps[0].length];
-        bfs(maps);
-        int answer = (maps[maps.length-1][maps[0].length-1]==1)?-1: maps[maps.length-1][maps[0].length-1];
-        return answer;
-    }
-    static void bfs(int[][] maps){
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{0,0});
-        visited[0][0]=true;
-        while(!q.isEmpty()){
-            int[] tmp = q.poll();
-            int x = tmp[0];
-            int y = tmp[1];
-            for(int i=0;i<4;i++){
-                int nx = x+dx[i];
-                int ny = y+dy[i];
-                if(nx>=0 && nx<maps.length && ny>=0 && ny<maps[0].length){ 
-                    if(!visited[nx][ny] && maps[nx][ny]==1){ // 방문하지 않았고, 길이라면
-                        maps[nx][ny]=maps[x][y]+1;
-                        visited[nx][ny]=true;
-                        q.offer(new int[]{nx,ny});
-                    }
+        isVisited[0][0] = true;
+        queue.add(new int[] {0, 0, 1});
+        
+        int cnt = 0;
+        int i = 0;
+        int j = 0;
+        
+        while(!queue.isEmpty()) {
+            int[] poll = queue.poll();
+            int x = poll[0];
+            int y = poll[1];
+            int c = poll[2];
+            if(x==n && y==m) return c;
+            for (int d = 0; d < 4; d++) {
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+                if (nx >= maps.length || nx < 0 || ny >= maps[0].length || ny < 0) continue;
+                if (isVisited[nx][ny]) continue;
+                if (maps[nx][ny] == 1) {
+                    queue.add(new int[] {nx, ny, c+1});
+                    isVisited[nx][ny] = true;
+                    cnt = c+1;
+                    
                 }
             }
         }
+        
+        if (i == n && j == m) {
+            return cnt;
+        }
+        
+        return -1;
+    }
+    
+    public int solution(int[][] maps) {
+        int answer = 0;
+        
+        answer = bfs(maps);
+        
+        return answer;
     }
 }
