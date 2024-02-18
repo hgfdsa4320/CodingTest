5a522e8984f2eb5cd261493a2ef617dee4af92d1
권순준 <80511046+hgfdsa4320@users.noreply.github.com>
2024-02-18 15:55:29 +0900

    [Silver I] Title: 회의실 배정, Time: 504 ms, Memory: 41908 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,27 +1,29 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
-public class Main{
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        int[][] meetings = new int[n][2];
-        for(int i=0;i<n;i++){
-            st = new StringTokenizer(br.readLine());
-            meetings[i][0]=Integer.parseInt(st.nextToken());
-            meetings[i][1]=Integer.parseInt(st.nextToken());
-        }
-    
-        Arrays.sort(meetings,(a,b)->(a[1]==b[1])?a[0]-b[0]:a[1]-b[1]);
-        int endTime = 0;
-        int answer = 0;
-        for(int[] meeting : meetings){
-            if(meeting[0]>=endTime){
-                endTime=meeting[1];
-                answer++;
-            }
-        }
-        System.out.println(answer);
-    }
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[1]==b[1])?a[0]-b[0]:a[1] - b[1]);
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			pq.offer(new int[] {s, e});
+		}
+		int now = 0;
+		int cnt = 0;
+		while (!pq.isEmpty()) {
+			int[] tmp = pq.poll();
+			if (tmp[0] >= now) {
+				now = tmp[1];
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+	}
 }
