190e34f96e3b1a979fc72c478eea231a47eb7197
권순준 <80511046+hgfdsa4320@users.noreply.github.com>
2024-03-04 10:04:58 +0900

    [Gold IV] Title: 최단경로, Time: 972 ms, Memory: 108484 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,50 +1,63 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
+
+class Node{
+    int to;
+    int weight;
+
+    public Node(int to, int weight) {
+        this.to = to;
+        this.weight = weight;
+    }
+}
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int v = Integer.parseInt(st.nextToken());
-        int e = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(br.readLine());
-        ArrayList<int[]>[] adjArr = new ArrayList[v+1];
-        for(int i=1;i<=v;i++) {
-            adjArr[i] = new ArrayList<>();
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(br.readLine());
+        int[] dist = new int[V + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[K] = 0;
+        ArrayList<Node>[] arr = new ArrayList[V + 1];
+        for (int i = 1; i <+ arr.length; i++) {
+            arr[i] = new ArrayList<>();
         }
-        for (int i = 0; i < e; i++) {
+        boolean[] visited = new boolean[V + 1];
+        for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            
-            adjArr[a].add(new int[] {b,c});
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            arr[u].add(new Node(v, w));
         }
-        int[] distance = new int[v + 1];
-        Arrays.fill(distance, Integer.MAX_VALUE);
-
-        // 현재 위치, 거리
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[1]-b[1]);
-        pq.offer(new int[] { k, 0 });
-        distance[k] = 0;
+        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.weight, b.weight));
+        pq.offer(new Node(K, dist[K]));
 
         while (!pq.isEmpty()) {
-            int[] tmp = pq.poll();
-            int now = tmp[0];
-            int dist = tmp[1];
-            
-            for(int j=0;j<adjArr[now].size();j++) { // 인접 리스트의 크기만큼 반복
-                int tmpDist = dist + adjArr[now].get(j)[1];
-                int tmpNode = adjArr[now].get(j)[0];
-                if (tmpDist < distance[tmpNode]) {
-                    distance[tmpNode] = tmpDist;
-                    pq.offer(new int[] {tmpNode,tmpDist});
+            Node n = pq.poll();
+            visited[n.to] = true;
+            for (int i = 0; i < arr[n.to].size(); i++) {
+                Node next = arr[n.to].get(i);
+                if (!visited[next.to] && n.weight + next.weight < dist[next.to]) {
+                    dist[next.to] = n.weight + next.weight;
+                    pq.offer(new Node(next.to, dist[next.to]));
+
                 }
             }
         }
-        for(int i=1;i<=v;i++) {
-            if(distance[i]==Integer.MAX_VALUE) System.out.println("INF");
-            else System.out.println(distance[i]);
+
+        for (int i = 1; i <= V; i++) {
+            System.out.println(dist[i] == Integer.MAX_VALUE ? "INF" : dist[i]);
         }
     }
+
+
 }
