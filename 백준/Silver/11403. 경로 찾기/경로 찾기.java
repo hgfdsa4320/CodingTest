d810b0dc45f61e18118ded7f3d7bc3b3870721bf
권순준 <80511046+hgfdsa4320@users.noreply.github.com>
2024-03-04 17:51:17 +0900

    [Silver I] Title: 경로 찾기, Time: 324 ms, Memory: 16244 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] adjcent = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                adjcent[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < N; k++) {
+                    if (adjcent[j][k] == 0) {
+                        if (adjcent[j][i] != 0 && adjcent[i][k] != 0) {
+                            adjcent[j][k] = adjcent[j][i] + adjcent[i][k];
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(adjcent[i][j]>0?1+" ":0+" ");
+            }
+            System.out.println();
+        }
+    }
+}
