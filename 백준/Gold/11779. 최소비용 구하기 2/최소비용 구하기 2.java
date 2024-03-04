51923280cb7e5f392c09249e88dd6775a0fa032f
권순준 <80511046+hgfdsa4320@users.noreply.github.com>
2024-03-04 17:14:00 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 504 ms, Memory: 51204 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -6,25 +6,24 @@ import java.util.*;
 class Type{
     int v;
     int weight;
-    String cities;
 
-    public Type(int v, int weight, String cities) {
+    public Type(int v, int weight) {
         this.v = v;
         this.weight = weight;
-        this.cities = cities;
     }
 
 }
 public class Main {
     static int n,m;
     static ArrayList<Type>[] arr;
-    static int[] dist;
+    static int[] dist, path;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         m = Integer.parseInt(br.readLine());
         arr = new ArrayList[n + 1];
         dist = new int[n + 1];
+        path = new int[n + 1];
         Arrays.fill(dist, Integer.MAX_VALUE);
         for (int i = 1; i <= n; i++) {
             arr[i] = new ArrayList();
@@ -34,7 +33,7 @@ public class Main {
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            arr[a].add(new Type(b, c, ""));
+            arr[a].add(new Type(b, c));
 
         }
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -42,28 +41,35 @@ public class Main {
         int e = Integer.parseInt(st.nextToken());
         dist[s] = 0;
         dijkstra(s, e);
+        Stack<Integer> stack = new Stack<>();
+        stack.push(e);
+        while (e != s) {
+            stack.push(path[e]);
+            e = path[e];
+        }
+
+        System.out.println(stack.size());
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop()+" ");
+        }
     }
 
     static void dijkstra(int s, int e) {
         PriorityQueue<Type> pq = new PriorityQueue<>((a, b) -> a.weight - b.weight);
-        pq.offer(new Type(s, 0, "" + s));
+        pq.offer(new Type(s, 0));
 
         while (!pq.isEmpty()) {
             Type t = pq.poll();
             if (t.v == e) {
                 System.out.println(t.weight);
-                String[] city = t.cities.split("-");
-                System.out.println(city.length);
-                for (int i = 0; i < city.length; i++) {
-                    System.out.print(city[i]+" ");
-                }
                 break;
             }
             for (int i = 0; i < arr[t.v].size(); i++) {
                 Type next = arr[t.v].get(i);
                 if (next.weight + t.weight < dist[next.v]) {
                     dist[next.v] = next.weight + t.weight;
-                    pq.offer(new Type(next.v, dist[next.v], t.cities+"-"+next.v));
+                    pq.offer(new Type(next.v, dist[next.v]));
+                    path[next.v] = t.v;
                 }
             }
         }
