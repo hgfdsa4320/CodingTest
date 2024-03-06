cb9b5b9b81b16f2928e5b4c9ae5a291c12701d88
권순준 <80511046+hgfdsa4320@users.noreply.github.com>
2024-03-06 10:48:51 +0900

    [Gold IV] Title: 운동, Time: 656 ms, Memory: 58292 KB -BaekjoonHub

---
+++ 백준/Gold/1956. 운동/운동.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[V + 1][V + 1];
+        for (int i = 1; i <= V; i++) {
+            Arrays.fill(arr[i], Integer.MAX_VALUE);
+        }
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            arr[a][b] = c;
+        }
+
+        for (int k = 1; k <= V; k++) {
+            for (int i = 1; i <= V; i++) {
+                for (int j = 1; j <= V; j++) {
+                    if (arr[i][k] == Integer.MAX_VALUE || arr[k][j] == Integer.MAX_VALUE) {
+                        continue;
+                    }
+                    arr[i][j] = Math.min(arr[i][j], arr[i][k] + arr[k][j]);
+                }
+            }
+        }
+
+        long ans = Long.MAX_VALUE;
+        for (int i = 1; i <= V; i++) {
+            for (int j = 1; j <= V; j++) {
+                if (i == j) {
+                    ans = Math.min(ans, arr[i][j]);
+                }else if (arr[i][j] != Integer.MAX_VALUE && arr[j][i] != Integer.MAX_VALUE) {
+                    ans = Math.min(ans, arr[i][j] + arr[j][i]);
+                }
+            }
+        }
+        if (ans == Integer.MAX_VALUE) {
+            System.out.println(-1);
+        } else {
+            System.out.println(ans);
+        }
+
+    }
+}
