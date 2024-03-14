333fca7ffd9614a3d397b75bd118a144400a261f
권순준 <80511046+hgfdsa4320@users.noreply.github.com>
2024-03-14 14:11:05 +0900

    [Silver III] Title: N과 M (1), Time: 276 ms, Memory: 21996 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,36 +1,39 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int n,m;
-	static int[] answer;
-	static boolean[] visited;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		answer = new int[m];
-		visited = new boolean[n+1];
-		permutation(0);		
-	}
-	static void permutation(int cnt) {
-		if(cnt == m) {
-			for(int i=0;i<cnt;i++) {
-				System.out.print(answer[i]+" ");
-			}
-			System.out.println();
-		}else {
-			for(int i=1;i<=n;i++) {
-				if(!visited[i]) {
-					answer[cnt] = i;
-					visited[i] = true;
-					permutation(cnt+1);
-					visited[i] = false;
-					
-				}
-			}
-		}
-	}
+  static int N, M;
+  static int[] arr;
+  static StringBuilder sb;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    boolean[] visited = new boolean[N + 1];
+    arr = new int[N + 1];
+    sb = new StringBuilder();
+    find(1, visited);
+    System.out.println(sb);
+  }
 
+  static void find(int now, boolean[] visited) {
+    if (now == M + 1) {
+      for (int i = 1; i <= M; i++) {
+        sb.append(arr[i]).append(" ");
+      }
+      sb.append("\n");
+      return;
+    }
+    for (int i = 1; i <= N; i++) {
+      if (!visited[i]) {
+        visited[i] = true;
+        arr[now] = i;
+        find(now + 1, visited);
+        visited[i] = false;
+      }
+    }
+  }
 }
