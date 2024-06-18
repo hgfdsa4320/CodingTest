1860fbaae6170ce9b74ad9322e8b196f9b808bc6
권순준 <80511046+hgfdsa4320@users.noreply.github.com>
2024-06-18 23:37:01 +0900

    [Gold IV] Title: 오큰수, Time: 804 ms, Memory: 159096 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,32 +1,31 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
-public class Main{
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] arr = new int[n];
-        for(int i=0;i<n;i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        Stack<int[]> stack = new Stack<>();
-        int[] answer = new int[n];
-        for(int i=0;i<n;i++){
-            while(!stack.isEmpty() && arr[i]>stack.peek()[1]){
-                int[] tmp = stack.pop();
-                answer[tmp[0]] = arr[i];
-            }
-            stack.push(new int[]{i,arr[i]});
-        }
-        while(!stack.isEmpty()){
-            int[] tmp = stack.pop();
-            answer[tmp[0]] = -1;
-        }
-        StringBuilder sb = new StringBuilder();
-        for(int i : answer){
-            sb.append(i).append(" ");
-        }
-        System.out.println(sb.toString());
-    }
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[N];
+		Arrays.fill(arr, -1);
+		Deque<int[]> d = new ArrayDeque<>();
+		for (int i = 0; i < N; i++) {
+			int tmp = Integer.parseInt(st.nextToken());
+			while (!d.isEmpty() && d.peekLast()[1] < tmp) {
+				arr[d.pollLast()[0]] = tmp;
+			}
+			d.offer(new int[] {i, tmp});
+		}
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			sb.append(arr[i]).append(" ");
+		}
+		System.out.println(sb);
+
+	}
 }
