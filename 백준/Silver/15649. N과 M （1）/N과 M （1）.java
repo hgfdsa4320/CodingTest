1963f4f99a35384b445a662030c89ea08936c774
권순준 <80511046+hgfdsa4320@users.noreply.github.com>
2024-06-20 16:32:28 +0900

    [Silver III] Title: N과 M (1), Time: 268 ms, Memory: 22372 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -4,36 +4,38 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-  static int N, M;
-  static int[] arr;
-  static StringBuilder sb;
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    N = Integer.parseInt(st.nextToken());
-    M = Integer.parseInt(st.nextToken());
-    boolean[] visited = new boolean[N + 1];
-    arr = new int[N + 1];
-    sb = new StringBuilder();
-    find(1, visited);
-    System.out.println(sb);
-  }
+	static int N, M;
+	static int[] arr;
+	static boolean[] visited;
+	static StringBuilder sb;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[M];
+		sb = new StringBuilder();
+		visited = new boolean[N + 1];
+		findArr(0);
+		System.out.println(sb);
+	}
 
-  static void find(int now, boolean[] visited) {
-    if (now == M + 1) {
-      for (int i = 1; i <= M; i++) {
-        sb.append(arr[i]).append(" ");
-      }
-      sb.append("\n");
-      return;
-    }
-    for (int i = 1; i <= N; i++) {
-      if (!visited[i]) {
-        visited[i] = true;
-        arr[now] = i;
-        find(now + 1, visited);
-        visited[i] = false;
-      }
-    }
-  }
+	static void findArr(int cnt) {
+		if (cnt == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(arr[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 1; i <= N; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				arr[cnt] = i;
+				findArr(cnt + 1);
+				visited[i] = false;
+			}
+		}
+	}
 }
