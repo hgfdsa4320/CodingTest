82842a251e3fddc9ab1fd3ae562e1715244bf5d2
권순준 <80511046+hgfdsa4320@users.noreply.github.com>
2024-10-15 21:07:43 +0900

    [Gold V] Title: LCS, Time: 124 ms, Memory: 18396 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -3,43 +3,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-//LCS(Longest Common Subsequence, 최장 공통 부분 수열)문제는 두 수열이 주어졌을 때, 모두의 부분 수열이 되는 수열 중 가장 긴 것을 찾는 문제이다.
-//
-// 예를 들어, ACAYKP와 CAPCAK의 LCS는 ACAK가 된다.
-// 012345
-// ACAYKP
-// CAPCAK
-// 입력
-// 첫째 줄과 둘째 줄에 두 문자열이 주어진다. 문자열은 알파벳 대문자로만 이루어져 있으며, 최대 1000글자로 이루어져 있다.
 public class Main {
-	static int[][] dp;
 	static String s1, s2;
-
-	// 부분 문자열 중 가장 긴 길이를 리턴하는 메서드
+	static int[][] dp;
+	static int answer;
 	static int lcs(int a, int b) {
-		if (a == -1 || b == -1) {
-			return 0;
+		for (int i = 1; i <= a; i++) {
+			for (int j = 1; j <= b; j++) {
+				if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+					dp[i][j] = dp[i - 1][j - 1] + 1;
+				} else {
+					dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+				}
+			}
 		}
-		if(dp[a][b]!=-1) {
-			return dp[a][b];
-		}
-		if (s1.charAt(a) == s2.charAt(b)) {
-			return dp[a][b] = lcs(a - 1, b - 1) + 1;
-		} else {
-			return dp[a][b] = Math.max(lcs(a - 1, b), lcs(a, b - 1));
-		}
+		return dp[a][b];
 	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		s1 = br.readLine();
 		s2 = br.readLine();
-		dp = new int[s1.length()][s2.length()];
-		for (int i = 0; i < s1.length(); i++) {
-			Arrays.fill(dp[i], -1);
-		}
-		lcs(s1.length() - 1, s2.length() - 1);
-
-		System.out.println(dp[s1.length() - 1][s2.length() - 1]);
+		answer = 0;
+		dp = new int[s1.length() + 1][s2.length() + 1];
+		System.out.println(lcs(s1.length(), s2.length()));
+		// for (int i = 1; i < s1.length() + 1; i++) {
+		// 	System.out.println(Arrays.toString(dp[i]));
+		// }
 	}
 }
