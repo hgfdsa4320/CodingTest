92dd74092bdb67004d9f4012f559230d5d6bf5bb
권순준 <80511046+hgfdsa4320@users.noreply.github.com>
2024-10-30 00:44:08 +0900

    [level 3] Title: 불량 사용자, Time: 20.93 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -3,65 +3,60 @@ import java.util.*;
 class Solution {
     static List<Integer>[] idList;
     static boolean[] visited;
-    static Set<Set<Integer>> uniqueCombinations;
-    static int bannedCount;
+    static Set<Integer> set;
+    static int M;
     
-    // 패턴이 일치하는지 확인하는 메서드
-    static boolean isMatch(String userId, String bannedId) {
-        if (userId.length() != bannedId.length()) return false;
-        for (int i = 0; i < bannedId.length(); i++) {
-            if (bannedId.charAt(i) != '*' && bannedId.charAt(i) != userId.charAt(i)) {
-                return false;
+    static boolean isMatch(String s1, String s2){
+        if(s1.length() != s2.length()){
+            return false;
+        }else{
+            for(int k=0;k<s1.length();k++){
+                char a = s1.charAt(k);
+                char b = s2.charAt(k);
+                if(a!='*' && a!=b){
+                    return false;
+                }
             }
         }
         return true;
     }
-
-    // 불량 사용자 ID와 일치하는 유저 ID를 idList에 저장
-    static void findMatchingIds(String[] user_id, String[] banned_id) {
-        for (int i = 0; i < banned_id.length; i++) {
-            for (int j = 0; j < user_id.length; j++) {
-                if (isMatch(user_id[j], banned_id[i])) {
+    
+    static void findBanIdList(String[] user_id, String[] banned_id){
+        for(int i=0;i<banned_id.length;i++){
+            for(int j=0;j<user_id.length;j++){
+                if(isMatch(banned_id[i],user_id[j])){
                     idList[i].add(j);
                 }
             }
         }
     }
     
-    // DFS를 통한 가능한 조합 찾기
-    static void dfs(int depth, Set<Integer> currentCombination) {
-        if (depth == bannedCount) {
-            uniqueCombinations.add(new HashSet<>(currentCombination));
+    static void dfs(int num,int bitMask){
+        if(num==M){
+            set.add(bitMask);
             return;
         }
-        
-        for (int userIdIndex : idList[depth]) {
-            if (!visited[userIdIndex]) {
-                visited[userIdIndex] = true;
-                currentCombination.add(userIdIndex);
-                dfs(depth + 1, currentCombination);
-                currentCombination.remove(userIdIndex);
-                visited[userIdIndex] = false;
+        for(int i=0;i<idList[num].size();i++){
+            int next = idList[num].get(i);
+            int bit = 1<<next;
+            if((bitMask & bit) == 0){
+                dfs(num+1,bitMask | bit);
             }
         }
     }
     
     public int solution(String[] user_id, String[] banned_id) {
-        bannedCount = banned_id.length;
-        idList = new ArrayList[bannedCount];
+        idList = new ArrayList[banned_id.length];
         visited = new boolean[user_id.length];
-        uniqueCombinations = new HashSet<>();
+        set = new HashSet<>();
+        M = banned_id.length;
         
-        for (int i = 0; i < bannedCount; i++) {
+        for(int i=0;i<banned_id.length;i++){
             idList[i] = new ArrayList<>();
         }
+        findBanIdList(user_id,banned_id);
+        dfs(0,0);
         
-        // 각 불량 사용자 ID에 맞는 사용자 ID 목록 생성
-        findMatchingIds(user_id, banned_id);
-        
-        // DFS를 사용하여 가능한 모든 조합 탐색
-        dfs(0, new HashSet<>());
-        
-        return uniqueCombinations.size();
+        return set.size();
     }
 }
