f9ade9a30863a29bfb1b976016737b11946a6faf
권순준 <80511046+hgfdsa4320@users.noreply.github.com>
2024-10-29 23:16:44 +0900

    [level 3] Title: 불량 사용자, Time: 101.37 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -3,68 +3,65 @@ import java.util.*;
 class Solution {
     static List<Integer>[] idList;
     static boolean[] visited;
-    static Set<Integer> set;
-    static int M;
+    static Set<Set<Integer>> uniqueCombinations;
+    static int bannedCount;
     
-    static void findBanIdList(String[] user_id, String[] banned_id){
-        for(int i=0;i<banned_id.length;i++){
-            String bId = banned_id[i];
-            for(int j=0;j<user_id.length;j++){
-                String uId= user_id[j];
-                boolean isPossible = true;
-                if(bId.length()!=uId.length()){
-                    isPossible = false;
-                }else{
-                    for(int k=0;k<bId.length();k++){
-                        char a = bId.charAt(k);
-                        char b = uId.charAt(k);
-                        if(a!='*' && a!=b){
-                            isPossible = false;
-                            break;
-                        }
-                    }
-                }
-                if(isPossible){
+    // 패턴이 일치하는지 확인하는 메서드
+    static boolean isMatch(String userId, String bannedId) {
+        if (userId.length() != bannedId.length()) return false;
+        for (int i = 0; i < bannedId.length(); i++) {
+            if (bannedId.charAt(i) != '*' && bannedId.charAt(i) != userId.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 불량 사용자 ID와 일치하는 유저 ID를 idList에 저장
+    static void findMatchingIds(String[] user_id, String[] banned_id) {
+        for (int i = 0; i < banned_id.length; i++) {
+            for (int j = 0; j < user_id.length; j++) {
+                if (isMatch(user_id[j], banned_id[i])) {
                     idList[i].add(j);
                 }
             }
         }
     }
     
-    static void dfs(int num,int now){
-        if(num==M){
-            String s = now+"";
-            String[] tmp = s.split("");
-            Arrays.sort(tmp);
-            s = "";
-            for(int i=0;i<tmp.length;i++){
-                s+=tmp[i];
-            }
-            set.add(Integer.parseInt(s));
+    // DFS를 통한 가능한 조합 찾기
+    static void dfs(int depth, Set<Integer> currentCombination) {
+        if (depth == bannedCount) {
+            uniqueCombinations.add(new HashSet<>(currentCombination));
             return;
         }
-        for(int i=0;i<idList[num].size();i++){
-            int next = idList[num].get(i);
-            if(!visited[next]){
-                visited[next] = true;
-                dfs(num+1,now*10+next);
-                visited[next] = false;
+        
+        for (int userIdIndex : idList[depth]) {
+            if (!visited[userIdIndex]) {
+                visited[userIdIndex] = true;
+                currentCombination.add(userIdIndex);
+                dfs(depth + 1, currentCombination);
+                currentCombination.remove(userIdIndex);
+                visited[userIdIndex] = false;
             }
         }
     }
     
     public int solution(String[] user_id, String[] banned_id) {
-        idList = new ArrayList[banned_id.length];
+        bannedCount = banned_id.length;
+        idList = new ArrayList[bannedCount];
         visited = new boolean[user_id.length];
-        set = new HashSet<>();
-        M = banned_id.length;
+        uniqueCombinations = new HashSet<>();
         
-        for(int i=0;i<banned_id.length;i++){
+        for (int i = 0; i < bannedCount; i++) {
             idList[i] = new ArrayList<>();
         }
-        findBanIdList(user_id,banned_id);
-        dfs(0,0);
         
-        return set.size();
+        // 각 불량 사용자 ID에 맞는 사용자 ID 목록 생성
+        findMatchingIds(user_id, banned_id);
+        
+        // DFS를 사용하여 가능한 모든 조합 탐색
+        dfs(0, new HashSet<>());
+        
+        return uniqueCombinations.size();
     }
 }
