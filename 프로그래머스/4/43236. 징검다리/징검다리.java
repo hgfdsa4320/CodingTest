eab64a4e4fd64c620f3503c3663e5061e1f53f14
권순준 <80511046+hgfdsa4320@users.noreply.github.com>
2024-11-01 02:08:20 +0900

    [level 4] Title: 징검다리, Time: 80.98 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -1,48 +1,47 @@
 import java.util.*;
 
 class Solution {
-    static boolean isOkay;
     
-    static int findNum(int[] rocks, int n,int distance){
-        int now = 0;
-        int cnt = 0;
-        for(int i=0;i<rocks.length;i++){
-            int diff = rocks[i]-now;
-            if(diff==n) isOkay = true;
-            if(diff<n){
-                cnt++;
-            }else{
-                now = rocks[i];
+    static int findRemovals(int[] rocks, int minDist, int distance) {
+        int now = 0;  // 현재 위치
+        int removals = 0;  // 제거할 돌의 수
+        
+        for (int rock : rocks) {
+            int diff = rock - now;
+            if (diff < minDist) {
+                removals++;  // 최소 거리보다 작으면 돌 제거
+            } else {
+                now = rock;  // 최소 거리 이상이면 현재 위치 갱신
             }
         }
-        if(distance-now<n){
-            cnt++;
+        
+        // 마지막 돌과 도착지 사이 거리 확인
+        if (distance - now < minDist) {
+            removals++;
         }
-        if(distance-now==n){
-            isOkay = true;
-        }
-        return cnt;
+        
+        return removals;
     }
     
     public int solution(int distance, int[] rocks, int n) {
-        int answer = 0;
+        Arrays.sort(rocks);  // 돌을 오름차순 정렬
+        
         int left = 1;
-        int right = 1000000000;
+        int right = distance;
+        int answer = 0;
         
-        Arrays.sort(rocks);
-        
-        while(left<=right){
-            int mid = (left+right)/2;
-            isOkay = false;
-            int num = findNum(rocks,mid,distance);
-            if(num<=n){
-                if(isOkay)
-                    answer = mid;
-                left = mid+1;
-            }else{
-                right = mid-1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int removals = findRemovals(rocks, mid, distance);
+            
+            if (removals <= n) {
+                answer = mid;  // 현재의 mid가 최소 거리로 가능한 경우 갱신
+                left = mid + 1;  // 최소 거리를 늘려보기 위해 left 증가
+            } else {
+                right = mid - 1;  // 돌을 너무 많이 제거했으면 거리 감소
             }
         }
+        
         return answer;
     }
 }
