2611082869458707b2e016647e066c74f280738e
권순준 <80511046+hgfdsa4320@users.noreply.github.com>
2024-10-31 16:03:51 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT ID,NAME,HOST_ID
+from PLACES
+where HOST_ID in (select HOST_ID
+                  from PLACES
+                  group by HOST_ID
+                  having count(*)>1)
+order by ID
