9a2f31b3dd6de1f1a7270c6a77342345476b5610
권순준 <80511046+hgfdsa4320@users.noreply.github.com>
2024-10-31 17:40:48 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 84.10 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -1,30 +1,22 @@
-/*
-dp?
-아니면 홀수, 짝수 누적합?
-
-*/
-import java.util.*;
-
 class Solution {
     public long solution(int[] sequence) {
-        long answer = 0;
-        long[][] dp = new long[sequence.length+1][2];
-        for(int i=1;i<=sequence.length;i++){
-            //짝수이면 
-            if(i%2==0){
-                dp[i][0] = Math.max(sequence[i-1],dp[i-1][0]+sequence[i-1]);
-                dp[i][1] = Math.max(-sequence[i-1],dp[i-1][1]-sequence[i-1]);
-            }else{
-                dp[i][0] = Math.max(-sequence[i-1],dp[i-1][0]-sequence[i-1]);
-                dp[i][1] = Math.max(sequence[i-1],dp[i-1][1]+sequence[i-1]);
-            }
+        long[][] dp = new long[sequence.length][2];
+        dp[0][0] = sequence[0];
+        dp[0][1] = -sequence[0];
+        long answer = Math.max(dp[0][0],dp[0][1]);
+        for(int i=1;i<sequence.length;i++){
+            int now = i%2==0?1:-1;
+            dp[i][0] = Math.max(sequence[i]*now,dp[i-1][0]+sequence[i]*now);
+            now = i%2==0?-1:1;
+            dp[i][1] = Math.max(sequence[i]*now,dp[i-1][1]+sequence[i]*now);
+            answer = Math.max(answer,Math.max(dp[i][0],dp[i][1]));
         }
-        for(int i=1;i<=sequence.length;i++){
-            for(int j=0;j<2;j++){
-                answer = Math.max(answer,dp[i][j]);
-            }
-        }
-        
         return answer;
     }
 }
+
+/**
+2 -1
+-2 1
+
+**/
