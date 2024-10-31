340913de9b4eefdad7860add2565925f00b5de38
권순준 <80511046+hgfdsa4320@users.noreply.github.com>
2024-10-31 15:48:00 +0900

    [level 3] Title: 징검다리 건너기, Time: 83.68 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,82 +1,35 @@
-import java.util.*;
-
 class Solution {
-    static int[] parent;
     
+    static int findNum(int[] stones, int n){
+        int num = 0;
+        int max = 0;
+        for(int i=0;i<stones.length;i++){
+            if(stones[i]-n<0){
+                num++;
+                max = Math.max(max,num);
+            }else{
+                num = 0;
+            }
+        }
+        return max;
+    }
     public int solution(int[] stones, int k) {
         int answer = 0;
-        parent = new int[stones.length]; 
-        for(int i=0;i<parent.length;i++){
-            parent[i] = i;
-        }  
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[1]==b[1]?a[0]-b[0]:a[1]-b[1]);
-        for(int i=0;i<stones.length;i++){
-            pq.offer(new int[]{i,stones[i]});
-        }
-        int now = pq.peek()[1];
-        boolean isOkay = false;
-        while(true){
-            int start = 0;
-            while(pq.peek()[1]==now){
-                int[] p = pq.poll();
-                int idx = p[0];
-                stones[idx] = 0;
-                if(idx>0 && stones[idx-1]==0){
-                    union(idx-1,idx);
-                }
-                if(idx+1<stones.length && stones[idx+1]==0){
-                    union(idx,idx+1);
-                }
-                
-                start = find(idx);
-                
-                if(start+k<=stones.length && find(start)==find(start+k-1)){
-                    isOkay = true; 
-                    answer = now;
-                    
-                    break;
-                }
+        int left = 1;
+        int right = 200000000;
+
+        while(left<=right){
+            //인원 수
+            int mid = (left+right)/2;
+            // 해당 인원수가 건널 때 가장 긴 공백 길이
+            int num = findNum(stones,mid);
+            if(num>=k){
+                right = mid-1;
+            }else{
+                answer = mid;
+                left = mid+1;
             }
-            if(isOkay){
-                break;
-            }
-            now = pq.peek()[1];
-            
         }
         return answer;
     }
-    static boolean union(int a,int b){
-        a = find(a);
-        b = find(b);
-        if(a==b) return false;;
-        parent[b] = a;
-        return true;
-    }
-    
-    static int find(int a){
-        if(parent[a] == a) return a;
-        return parent[a] = find(parent[a]);
-    }
 }
-
-/**
-20만개
-우선순위 큐[인덱스, 값] -> 값이 작은 순, 같으면 인덱스가 작은 순
-
-K : 1이상 20만 이하 
-
-3	3
-1 1 2 2 2 3 4 4 5 5 
-1 1 1번
-2 2 2 2 번
-3 3번
-우선 순위 큐 이거 다 -> logN 시간 복잡도를 가짐 -> 
-현재 : 우선순위큐 peek();
-while(true){ 최대 -> N번
-    if(우선순위큐 - 현재==0) 다 빼기 -> logN* 빼는 횟수만큼
-    
-    여기서 배열 확인 -> 20만번 -> 이거를 다른 로직으로 바꿔야 될 거같은데 
-    흠 유니온파인드 써야되나
-    -> 유니온 파인드 
-}
-**/
