0fee3525118484c3248065a2891e07bc0bfa98c3
권순준 <80511046+hgfdsa4320@users.noreply.github.com>
2024-10-31 15:58:01 +0900

    [level 3] Title: 입국심사, Time: 65.56 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,42 +1,27 @@
-/**
-1. 이분 탐색으로 시간 범위 줄이기
-2. 해당 시간안에 입국 심사를 할 수 있는 인원이 n이라면
--> 인원은 count = 시간/times[i]로 파악
-3. 최소 시간은 
--> 시간 - (시간%time[i]의 최솟값)
-**/
-
-import java.util.*;
-
 class Solution {
-    static long min;
+    
+    static long findNum(int[] times, long mid){
+        long cnt = 0;
+        for(int i=0;i<times.length;i++){
+            cnt+=mid/times[i];
+        }
+        return cnt;
+    }
     public long solution(int n, int[] times) {
         long answer = 0;
-        long start = 0;
-        long end = 1000000000000000000L;
+        long left =1;
+        long right = 100000000000000L;
         
-        while(start<=end){
-            long mid = (start+end)/2;
-            long num = findNum(n, times, mid);
-            if(num==n){
-                end = mid-1;
-            }else if(num>n){
-                end = mid-1;
+        while(left<=right){
+            long mid = (left+right)/2;
+            long num = findNum(times,mid);
+            if(num>=n){
+                answer = mid;
+                right = mid-1;
             }else{
-                start = mid+1;
+                left = mid+1;
             }
         }
-        
-        return start;
-    }
-    static long findNum(int n, int[] times, long time){
-        min = Long.MAX_VALUE;
-        long count = 0;
-        for(int i=0;i<times.length;i++){
-            count += time/times[i];
-            min = Math.min(min,time%times[i]);
-        }
-        return count;
+        return answer;
     }
 }
-
