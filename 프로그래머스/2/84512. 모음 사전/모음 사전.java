7e9998084dab512e29ae828c8e9b4308ce30eb2f
권순준 <80511046+hgfdsa4320@users.noreply.github.com>
2024-11-22 23:07:32 +0900

    [level 2] Title: 모음 사전, Time: 25.88 ms, Memory: 95.7 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,27 +1,24 @@
 import java.util.*;
 
 class Solution {
-    static String[] w;
     static int answer;
-    static int time;
-    static void dfs(String now,String word){
-        if(now.equals(word)){
-            answer = time;
-            return;
-        } 
+    static char[] w;
+    static int cnt;
+    static void findNum(String now, String word){
+        if(now.equals(word))
+            answer = cnt;
         if(now.length()<5){
             for(int i=0;i<5;i++){
-                time++;
-                dfs(now+w[i],word);
+                cnt++;
+                findNum(now+w[i],word);
             }
         }
     }
-    
     public int solution(String word) {
         answer = 0;
-        time = 0;
-        w = new String[]{"A","E","I","O","U"};
-        dfs("",word);
+        cnt = 0;
+        w = new char[]{'A','E','I','O','U'};
+        findNum("",word);
         return answer;
     }
 }
